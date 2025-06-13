a681c0db06ee785e3cba852b39d08b73d92994fb
tanveer2417 <tanviafifa@gmail.com>
2025-06-13 18:49:03 +0530

    initial commit

--- setup.py
+++ setup.py
@@ -4,21 +4,24 @@ setup(
     name='reconx',
     version='1.0',
     packages=find_packages(),
+    include_package_data=True,
     entry_points={
         'console_scripts': [
-            'reconx=reconx.main:main',
+            'reconx = reconx.main:main',
         ],
     },
     install_requires=[
         'requests',
         'dnspython',
-        'python-whois'
+        'python-whois',
+        'colorama',
+        'beautifulsoup4'
     ],
-    author='Mariya Fareed',
+    author='Mariya Fareed''Ruheena Begum''Tanveer Fatima', 
     description='Automated OSINT and Recon CLI tool',
     long_description='A Python tool to automate OSINT tasks such as Google/GitHub dorks, metadata extraction, WHOIS, DNS, IP info, and more.',
     long_description_content_type='text/markdown',
-    url='https://github.com/mariyafareed/reconx',
+    url='https://github.com/tanveer2417/ReconX',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
